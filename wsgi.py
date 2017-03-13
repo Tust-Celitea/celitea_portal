b39dd28e556e2f8f60ebbabbf883165d5a0960cb
KenOokamiHoro <horo@yoitsu.moe>
2017-03-13 20:28:34 +0800

    update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment ,Group
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 
 def wsgi_create_app():
